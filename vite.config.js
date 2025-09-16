640c132d1b497a451e07cd173a4534d6dbde3749
shantanux1718 <160618075+shantanux1718@users.noreply.github.com>
2025-09-16 22:47:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",  // IMPORTANT: makes asset paths relative, fixes MIME errors on Netlify
+  base: "./",  // IMPORTANT: makes asset paths relative, fixes MIME errors on Netlify 
 });
